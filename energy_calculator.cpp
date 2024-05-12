9216e5cb8d6c980843c624a6c467bd4d3a605acd
Tarcillo Gaziri <tarcillo@Tarcillos-MacBook-Air.local>
2024-05-11 21:16:05 -0300

    Add first file

---
+++ energy_calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+int main(int argc, char const *argv[])
+{	
+
+	char *pdb;
+	char *topol;
+
+
+	for (int i = 0; i++; i < argc)
+	{
+		if (argv[i] == "-pdb")
+		{
+			
+		}
+	}
+
+
+	return 0;
+}
